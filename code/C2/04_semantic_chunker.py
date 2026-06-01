20615e3ccc4ca37827abe447f05d7b0f007ca50a
EPxiaoma <599902085@qq.com>
2026-06-01 19:39:49 +0800

    C2：语义分块

--- code/C2/04_semantic_chunker.py
+++ code/C2/04_semantic_chunker.py
@@ -1,26 +1,30 @@
-from langchain_experimental.text_splitter import SemanticChunker
-from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.document_loaders import TextLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_experimental.text_splitter import SemanticChunker
 
+# 1. 文档加载
+loader = TextLoader("../../data/C2/txt/蜂医.txt", encoding="utf-8")
+docs = loader.load()
+
+# 2. 创建嵌入模型
 embeddings = HuggingFaceEmbeddings(
     model_name="BAAI/bge-small-zh-v1.5",
     model_kwargs={'device': 'cpu'},
     encode_kwargs={'normalize_embeddings': True}
 )
 
-# 初始化 SemanticChunker
+# 3. 初始化递归字符分块器
 text_splitter = SemanticChunker(
     embeddings,
     breakpoint_threshold_type="percentile" # 也可以是 "standard_deviation", "interquartile", "gradient"
 )
 
-loader = TextLoader("../../data/C2/txt/蜂医.txt", encoding="utf-8")
-documents = loader.load()
+# 4. 执行分块
+chunks  = text_splitter.split_documents(docs)
 
-docs = text_splitter.split_documents(documents)
-
-print(f"文本被切分为 {len(docs)} 个块。\n")
+# 5. 打印结果
+print(f"文本被切分为 {len(chunks )} 个块。\n")
 print("--- 前2个块内容示例 ---")
-for i, chunk in enumerate(docs[:2]):
+for i, chunk in enumerate(chunks [:2]):
     print("=" * 60)
     print(f'块 {i+1} (长度: {len(chunk.page_content)}):\n"{chunk.page_content}"')
